aa30802f537c6c21992788a2c6b73a0666dcce29
Semen Larin <navigatorsvs@gmail.com>
2021-06-12 16:18:39 +0300

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Рваная футболка',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:1800,
+            brand:'Херайк',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Бомжовская толстовка',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:1900,
+            brand:'Nike',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Рваные носки',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:1290,
+            brand:'Nike',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Рваные трусы',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:300,
+            brand:'Митрофаново',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Шапка универсал',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:190,
+            brand:'мирсаново',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'Шайтан шапка',
+            category:'Shirts',
+            image:'/images/p6.jpg',
+            price:960,
+            brand:'Казаново',
+            rating:4.5,
+            NumReviews:10,
+            description:'high quality product'
+        },
+        
+    ]
+}
+export default data;
